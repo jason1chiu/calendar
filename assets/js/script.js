de1ec4225b728e92db6a38c169f3116fd89f0fcb
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-01-22 11:15:39 -0500

    added and remove  extra comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,17 @@
 // in the html.
 $(function () {
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
+  // Added a listener for click events on the save button 
   $('.saveBtn').on('click', savePlanner);
 
+  // Function to save the user input to local storage by "hour-x" id of the time-block containing the button that was clicked.
   function savePlanner() {
     var time = $(this).parent().attr('id').split('-')[1];
     var plan = $(this).siblings('.description').val().trim();
     localStorage.setItem(time, plan);
   }
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // Function to pply the past, present, or future class to each time block by comparing the id to the current hour.
 
   $('.time-block').each(function() {
     var presentHourString = dayjs().format("HH");
@@ -43,16 +34,14 @@ $(function () {
     }
   }); 
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  // Function to set the values of the corresponding textarea elements using id hour-x
     
     for (var i = 9; i <= 17; i++) {
       var dailyPlan = localStorage.getItem(i);
       $('#hour-'+i).children('.description').val(dailyPlan);
     }
 
-    // TODO: Add code to display the current date in the header of the page.
+    // Function to display the current date in the header of the page.
     var today = dayjs();
     $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
 
